539250d3ce56d36d43829ca813a068adc3d3cab7
Mahmoud-Ehab <mo.ehab150@gmail.com>
2024-08-07 16:08:26 +0300

    PostPage has been responsive

--- src/fragments/PostPage.vue
+++ src/fragments/PostPage.vue
@@ -11,11 +11,11 @@ document.scrollingElement?.scroll(0, 0)
 </script>
 
 <template>
-  <div class="w-full min-h-screen px-12">
-    <h1 class="text-5xl text-primary">{{ post?.title }}</h1>
+  <div class="w-full min-h-screen px-2 md:px-12">
+    <h1 class="text-pretty text-4xl md:text-5xl text-primary">{{ post?.title }}</h1>
     <label class="p-2 text-lg text-secondary">By {{ post?.blogger_username }}</label
     ><br />
     <label class="p-2 text-md text-secondary">{{ post?.date }}</label>
-    <p class="text-xl text-base-100 p-4 text-justify">{{ post?.content }}</p>
+    <p class="text-lg md:text-xl text-base-100 p-4 text-justify">{{ post?.content }}</p>
   </div>
 </template>
