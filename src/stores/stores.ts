130953796942be9e595e11a35f9fb6d9e703fd2b
Mahmoud E. Abdelsalam <mo.ehab150@gmail.com>
2024-10-20 21:29:06 +0300

    initial commit

--- src/stores/stores.ts
+++
@@ -1,5 +0,0 @@
-import { useUserStore } from './UserStore'
-import { usePostStore } from './PostStore'
-import { useBookmarkStore } from './BookmarkStore'
-
-export { useUserStore, usePostStore, useBookmarkStore }
