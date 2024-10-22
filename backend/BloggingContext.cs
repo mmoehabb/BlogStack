6d46546a5b52c7c09bdf69f530c5540502b3eca1
Mahmoud E. Abdelsalam <mo.ehab150@gmail.com>
2024-10-23 01:24:40 +0300

    DbInitializer removed.

--- backend/BloggingContext.cs
+++ backend/BloggingContext.cs
@@ -23,30 +23,3 @@ public class BloggingContext : DbContext
     base.OnModelCreating(modelBuilder);
   }
 }
-
-public static class DbInitializer 
-{
-  public static void Initialize(BloggingContext context) 
-  {
-    if (context.Writers.Any()) {
-      return; // Data has been seeded.
-    }
-    var writers = new Writer[] {
-      new Writer{Username="Smith",Password="123321",DisplayName="SmithTheWriter",Email="smith@example.com"},
-      new Writer{Username="Jack",Password="123123",DisplayName="JackBlogs",Email="jack@example.com"},
-      new Writer{Username="John",Password="123456",DisplayName="JustJohn",Email="john@example.com"},
-    };
-    context.Writers.AddRange(writers);
-
-    var posts = new Post[] {
-      new Post{ Title="My first post.", Content="Just discovering the application!", Writer=writers[0] },
-    };
-    context.Posts.AddRange(posts);
-
-    var bookmarks = new Bookmark[] {
-      new Bookmark{ Writer=writers[1], Post=posts[0] }
-    };
-    context.Bookmarks.AddRange(bookmarks);
-    context.SaveChanges();
-  }
-}
