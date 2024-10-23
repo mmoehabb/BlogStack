1d564d89a297241adac2ebc598ba2b0d8890fc46
M. E. Abdelsalam <mo.ehab150@gmail.com>
2024-10-24 02:02:14 +0300

    hash func key has been hard coded.

--- backend/Hasher.cs
+++ backend/Hasher.cs
@@ -4,11 +4,11 @@ using System.Security.Cryptography;
 // credit: https://medium.com/@chris.mckee/how-to-match-hmacsha256-between-c-python-f7b42d01cbf5
 public static class Hasher 
 {
-  public static string HmacSHA256(string key, string data)
+  public static string HmacSHA256(string data)
   {
       string hash;
       ASCIIEncoding encoder = new ASCIIEncoding();
-      Byte[] code = encoder.GetBytes(key);
+      Byte[] code = encoder.GetBytes("justanarbitrarysecretkey");
       using (HMACSHA256 hmac = new HMACSHA256(code))
       {
           Byte[] hmBytes = hmac.ComputeHash(encoder.GetBytes(data));
